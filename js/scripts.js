ab05b70d1067d39f990cdb3b19a57fa9a574e1c2
kiluhima <kiluhima@gmail.com>
2016-07-15 02:58:17 -0400

    scrollbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,10 @@
 //hello
-var controller = new ScrollMagic.Controller();
-var scene = new ScrollMagic.Scene({
-        triggerElement: "#intro-banner",
-        duration: 300
-    })
-    .setTween("#animate1", 0.5, {
-        backgroundColor: "green",
-        scale: 2.5
-    }) // trigger a TweenMax.to tween
-    .addIndicators({
-        name: "1 (duration: 0)"
-    }) // add indicators (requires plugin)
-    .addTo(controller);
-
 $(window).scroll(function () {
-    if ($(".navbar").offset().top > 50) {
+    if (window.pageYOffset > 50) {
+        //small scroll bar
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
+        //big scroll bar
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
 });
