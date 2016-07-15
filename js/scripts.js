76f04effeb533e5d6cf904d28d8a62ffef1bd45c
kiluhima <kiluhima@gmail.com>
2016-07-15 02:33:34 -0400

    nav js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
 //hello
+var controller = new ScrollMagic.Controller();
+var scene = new ScrollMagic.Scene({
+        triggerElement: "#intro-banner",
+        duration: 300
+    })
+    .setTween("#animate1", 0.5, {
+        backgroundColor: "green",
+        scale: 2.5
+    }) // trigger a TweenMax.to tween
+    .addIndicators({
+        name: "1 (duration: 0)"
+    }) // add indicators (requires plugin)
+    .addTo(controller);
+
+$(window).scroll(function () {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
