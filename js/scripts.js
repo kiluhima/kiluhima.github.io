b52c03e18e5b004dda5276de6a42afa2aee46400
nodcah <nodcah@gmail.com>
2016-07-16 13:35:44 -0400

    Stuff

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,8 @@ $(window).scroll(function () {
     if (window.pageYOffset > 50) {
         //small scroll bar
         $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else if (window.pageYOffset > 50 && window.pageYOffset < 100) {
+
     } else {
         //big scroll bar
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
