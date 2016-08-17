1d6c9a83a1a5f02c5489cf39b3398ce6eaff6366
nodcah <nodcah@gmail.com>
2016-08-17 14:11:17 -0400

    Working....

--- js/scripts.js
+++ js/scripts.js
@@ -13,22 +13,19 @@ if (window.innerWidth < 992) {
 function sizing() {
     if (window.innerWidth < 992) {
         //if mobile, don't change anything
-        document.getElementsByClassName("nav-wrapper")[0].style.backgroundColor = "rgba(50,50,50,.75)";
-        document.getElementById("nav-background").style.backgroundColor = "rgba(100,100,100,0)";
+
+        document.getElementsByTagName("nav")[0].style.backgroundColor = "rgba(100,100,100,0)";
     } else if (window.pageYOffset > 50) {
         //small scroll bar
-        document.getElementById("nav-background").style.backgroundColor = "rgba(50,50,50,.5)";
-        document.getElementById("nav-background").style.height = "4em";
-        //document.getElementById("desktop").style.paddingTop = "1em";
+        document.getElementsByTagName("nav")[0].style.backgroundColor = "rgba(50,50,50,.5)";
+        document.getElementsByTagName("nav")[0].style.height = "4em";
         document.getElementById("logo").src = "images/Website_Logo_Small.svg";
-        document.getElementsByClassName("nav-wrapper")[0].style.backgroundColor = "rgba(50,50,50,0)";
+
 
     } else if (window.pageYOffset === 0) {
         //big scroll bar
-        document.getElementById("nav-background").style.backgroundColor = "rgba(100,100,100,0)";
-        document.getElementsByClassName("nav-wrapper")[0].style.backgroundColor = "rgba(50,50,50,0)";
-        document.getElementById("nav-background").style.height = "8em";
-        //document.getElementById("desktop").style.paddingTop = "2em";
+        document.getElementsByTagName("nav")[0].style.backgroundColor = "rgba(100,100,100,0)";
+        document.getElementsByTagName("nav")[0].style.height = "8em";
         document.getElementById("logo").src = "images/Website_Logo.svg";
     }
 }
