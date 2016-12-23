f99ef0e36bebcad87d21fe021d8ff56208faa1e4
nodcah <nodcah@gmail.com>
2016-12-22 23:41:33 -0500

    Changed color of nav bar

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,17 @@ $('.button-collapse').sideNav({
 //fancy changing scroll navbar
 "use strict";
 
+const navColor = "rgba(31, 31, 31, 0.75)"
+
 function sizing() {
     if (window.innerWidth < 992) {
         //if mobile, don't change anything
         document.getElementsByTagName("nav")[0].style.height = "4em";
-        document.getElementsByTagName("nav")[0].style.backgroundColor = "rgba(50,50,50,.8)";
+        document.getElementsByTagName("nav")[0].style.backgroundColor = navColor;
         document.getElementsByTagName("nav")[0].style.lineHeight = "4em";
     } else if (window.pageYOffset > 50) {
         //small scroll bar
-        document.getElementsByTagName("nav")[0].style.backgroundColor = "rgba(50,50,50,.8)";
+        document.getElementsByTagName("nav")[0].style.backgroundColor = navColor;
         document.getElementsByTagName("nav")[0].style.height = "4em";
         document.getElementsByTagName("nav")[0].style.lineHeight = "4em";
         document.getElementById("logo").style.height = "3.5em";
@@ -22,7 +24,7 @@ function sizing() {
         document.getElementById("logo").src = "images/Website_Logo_Small.svg";
     } else if (window.pageYOffset === 0) {
         //big scroll bar
-        document.getElementsByTagName("nav")[0].style.backgroundColor = "rgba(100,100,100,0)";
+        document.getElementsByTagName("nav")[0].style.backgroundColor = "transparent";
         document.getElementsByTagName("nav")[0].style.height = "10em";
         document.getElementsByTagName("nav")[0].style.lineHeight = "8em";
         document.getElementById("big-nav-ul").style.paddingTop = "2em";
